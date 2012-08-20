ac8c808a52ed8461aee8bf1ca19a9eff76920195
Nara Laurence <naralaurence@nara.com>
2012-08-20 14:14:38 +0100

    updated pages controller

---
+++ application/classes/controller/pages.php
@@ -0,0 +1,51 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Controller: Pages. //@todo Pages have to read via the api from the cms
+ *
+ * @package    Feowl/Home
+ * @author     Feowl Team
+ * @copyright  (c) 2012 Feowl Team
+ * @license    http://feowl.tumblr.com/
+ */     
+class Controller_Pages extends Controller_Template {
+	
+	//define the template to use
+	public $template = 'template/template.tpl';
+
+	public function action_index() {
+		//default page lands to about
+		$this->about();
+	}
+	public function action_about()
+	{
+	$this->template->content = View::factory('pages/about.tpl');
+	}
+	
+	public function after(){
+		// Adds optional stylesheet files in an array
+		$this->template->files_stylesheet = array(
+			url::base()."assets/css/jquery.qtip.css",	
+			url::base()."assets/less/jQRangeSlider.less",	
+		);
+
+		// Adds optional javascript files in an array
+		$this->template->files_javascript = array(	
+			url::base()."assets/js/handlebars.js",
+			url::base()."assets/js/chroma.min.js",
+			url::base()."assets/js/kartograph.js",
+			url::base()."assets/js/raphael.min.js",
+			url::base()."assets/js/jquery-ui-1.8.16.custom.min.js",	
+			url::base()."assets/js/jquery.qtip.min.js",				
+			url::base()."assets/js/jQAllRangeSliders-min.js",
+			url::base()."assets/js/global.js",
+			url::base()."assets/js/script-explore.js",
+		);
+		$this->template->active_about = "active";
+		$alert = Session::instance()->get_once('alert');
+		if(isset($alert)){
+			$this->template->alert = $alert;
+		}
+		parent::after();		
+	}
+
+} // End Home
