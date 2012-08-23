d93bbd18312f2a00bc3a6e35bd6ab467b641cbb1
Nara Laurence <naralaurence@nara.com>
2012-08-24 00:11:02 +0100

    1st version of Javascript to work with new mockup

--- assets/js/script-contribute.js
+++ assets/js/script-contribute.js
@@ -1,50 +1,65 @@
 $(document).ready(function() {
     //initialize
-	$(".endContainer").hide()
-   // $("").attr('disabled','disabled')
-        $('.controls input:radio').change(function () {
+	//btn-primary 
+	$(".endContainer, .grid2, .grid3").hide()
+    $(".contribute1, .contribute1-2, .contribute2, .contribute2-1, .contribute3, .contribute3-1").attr('disabled','disabled')
 
-        a = $('input[name=contribute]:checked').val();
-		 
-        	if(a=="true")
-			{
-			//@todo, generalize this process
-			 $(".contribute1").addClass("current")
-			 $(".contribute1").removeAttr('disabled')
-			 $(".contribute1").change(function(){
+        $(".contribute").change(function(){
+		//ger multiple powercut value
+		   num = $(".contribute option:selected").val()
 			 $(".success-tick").show()
-			 $(".contribute1-1").removeAttr('disabled')
+			 $(".contribute1").removeAttr('disabled')
 			  $(".contribute1").removeClass("current")
-			 $(".contribute1-1").addClass("current")
+			 $(".contribute1").addClass("current")
+			 //show grids
+		      if(num==2)
+			  $(".grid2").show();
+			  if(num==3)
+			  $(".grid2,.grid3").show();
+		    });
+        
+			//@todo, generalize this process
+			 $(".contribute1").change(function(){
+			 $(".contribute1-2").removeAttr('disabled')
+			  $(".contribute1").removeClass("current")
+			 $(".contribute1-2").addClass("current")
 			 })
-			  $(".contribute1-1").change(function(){
-			   $(".success-tick1-1").show()
+			  $(".contribute1-2").change(function(){
+			  selected = $(".contribute1-2 option:selected").val()
+
+			  //fill 2-1,3-1 with values of 1-1
+			  $(".contribute2-1,.contribute3-1").val( selected ).attr('selected',true);
+
+			   $(".success-tick1").show()
 			  $(".contribute2").removeAttr('disabled')
-			  $(".contribute1-1").removeClass("current")
+			  $(".contribute1-2").removeClass("current")
 			 $(".contribute2").addClass("current")
 			 });
 			  $(".contribute2").change(function(){
+			   $(".contribute2-1").removeAttr('disabled')
+			  $(".contribute2").removeClass("current")
+			 $(".contribute2-1").addClass("current")
+			 });
+			   $(".contribute2-1").change(function(){
 			   $(".success-tick2").show()
 			   $(".contribute3").removeAttr('disabled')
-			  $(".contribute2").removeClass("current")
+			  $(".contribute2-1").removeClass("current")
 			 $(".contribute3").addClass("current")
 			 });
-			 $(".contribute3").change(function(){
-			   $(".success-tick3").show()
+			    $(".contribute3").change(function(){
+			   $(".contribute3-1").removeAttr('disabled')
+			  $(".contribute3").removeClass("current")
+			 $(".contribute3-1").addClass("current")
 			 });
-			 }else if( a=="false"){
-			 //disable all other fields and goto last step
-			//$(".contribute1-1, .contribute2, .contribute1").attr('disabled','disabled').empty()
-			$(".contribute1").removeClass("current")
-			$(".contribute3").addClass("current")
-			$(".contribute3").removeAttr('disabled')			 
-			$(".contribute3").addClass("false")	
-				 $(".contribute3").change(function(){
+			 $(".contribute3-1").change(function(){
 			   $(".success-tick3").show()
+			    $(".contribute3-1").removeClass("current")
+				$(".contribute-submit").addClass("btn-primary")
 			 });
-				}
+			 
 
-});
+
+
 			 		 
 		 //handle submit
 		 $("form#contribute").submit(function(){
