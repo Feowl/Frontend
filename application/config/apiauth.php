fd02aa028166d7d0fae2ed59946f6213205c09b7
Pierre Romera <hello@pirhoo.com>
2013-07-01 15:28:20 +0200

    New conf file and tmp directoru

---
+++ application/config/apiauth.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+  return array(
+    "default" => array(
+      "username" => $_ENV["API_USERNAME"],
+      "api_key"  => $_ENV["API_KEY"]
+    )
+);
---
+++ application/config/sendgrid.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+    'username'  => $_ENV["SENDGRID_USERNAME"],
+    'password'  => $_ENV["SENDGRID_PASSWORD"]
+);  
